e754ab9ecde29efc0de8d24fd1dcad1d79cb11f8
vikash patel <vikash.patel@getcogno.ai>
2022-12-27 08:20:17 +0530

    livechat bug fixed

--- CognoChatbot-iOS/CognoChatbot-iOS/Constants.swift
+++ CognoChatbot-iOS/CognoChatbot-iOS/Constants.swift
@@ -12,8 +12,9 @@ public class Constants {
     public static var botUrl = ""
     public static var botId = ""
     public static var accessToken = ""
-    public static var category = ""
     public static var isTokenVerify = false
     public static let tokenVerificationUrl = "/chat/verify-access-token/"
+    public static let livechatSessionIDVerificationUrl = "/livechat/get-livechat-session-expire/"
+    public static var mobileChatbotSessionID = ""
+    public static var mobileLiveChatSessionID = ""
 }
-
